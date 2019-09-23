594998b542fc7b2456de14b2f5d1111fc2a90734
sarah <s.j.condon01@gmail.com>
2019-09-23 00:38:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+ range + (2..(number -1)).to_a
+ if number < 2 
+   return false
+ else
+   range.each do |i|
+     if number % i == 0
+       return false
+  end
+end
+  return true
+end
